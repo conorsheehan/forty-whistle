5035a3ffe0bed27ee4e9f38a85d1987249b74e48
Conor Sheehan <conor@cantinaconsulting.com>
2015-10-23 10:55:02 -0400

    Clean up drawPath

--- script/main.js
+++ script/main.js
@@ -1,5 +1,4 @@
-function simulatePathDrawing(path) {
-  // var path = document.querySelector('.squiggle-animated path');
+function drawPath(path) {
   var length = path.getTotalLength();
   // Clear any previous transition
   path.style.transition = path.style.WebkitTransition = 'none';
@@ -9,19 +8,12 @@ function simulatePathDrawing(path) {
   // Trigger a layout so styles are calculated & the browser
   // picks up the starting position before animating
   path.getBoundingClientRect();
-  // Define our transition
-  // path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 1.5s ease-in-out';
+  // Reset transition
   path.style.transition = path.style.WebkitTransition = '';
   // Go!
-  path.style.strokeDashoffset = '0';
-  path.style.strokeWidth = '1px';
-  path.style.strokeOpacity = '1';
-  path.style.fill = 'rgba(0,0,0,1)';
+  path.style.strokeDashoffset = '';
+  $(path).attr('class', 'shown');
 }
-
-// var chars = $('.svg-animated path').on('mouseover', function(e) {
-//   simulatePathDrawing(this)
-// })
 
 function drawName() {
   var $name = $('.svg-name');
@@ -29,7 +21,7 @@ function drawName() {
   $name.find('path').each(function() {
     var letterPath = this;
     setTimeout(function() {
-      simulatePathDrawing(letterPath);
+      drawPath(letterPath);
     }, i * 150 + 250);
     i += 1;
   });
